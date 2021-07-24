094ba004390d97dfc561f4245a7f58a72e0bdd52
Thushar <thushar.work@gmail.com>
2021-07-24 10:56:52 +0530

    Add input and item list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import AddItems from './components/AddItems';
+import Items from './components/Items';
 
 function App() {
+  const [itemList, setItemList] = useState([
+    {
+      id: 1,
+      item: 'Milk ',
+      noOfItems: 2,
+      priceOfEachItem: 20,
+    },
+    {
+      id: 2,
+      item: 'Curd',
+      noOfItems: 4,
+      priceOfEachItem: 25,
+    },
+  ]);
+
+  const addItem = (item) => {
+    const id = Math.floor(Math.random() * 1000000) + 1;
+    const newItem = { id, ...item };
+    setItemList([...itemList, newItem]);
+  };
+
+  const deleteItem = (id) => {
+    setItemList(itemList.filter((item) => item.id !== id));
+  };
+
+  const updateItem = (item) => {
+    const updatedList = itemList.map((itm) => (itm.id === item.id ? { ...item } : itm));
+    setItemList(updatedList);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Grocery List </h1>
+      <AddItems addItem={addItem} />
+      <Items items={itemList} onDelete={deleteItem} onUpdate={updateItem} />
     </div>
   );
 }
